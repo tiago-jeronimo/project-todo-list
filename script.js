afd5710e0595059393ab3bad213004de768bfbb4
Tiago Jerônimo <tiago.batmansz@gmail.com>
2022-01-28 14:46:38 -0300

    Requisito 7 e 8

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
+// const creatBtnDelet = document.createElement('button');
+    // creatBtnDelet.classList.add('.btnDelet');
+    // creatBtnDelet.innerText=""
+    // criaItemLista.appendChild(creatBtnDelet)
+
+
+
 const inputTextoTarefa = document.querySelector('#texto-tarefa');
 const btnCriarTarefa = document.querySelector('#criar-tarefa');
-const itemList =document.querySelector('.item-lista');
-const btnDelet = document.querySelector('.btnDelet')
+const itemList = document.getElementsByClassName('.item-lista');
+const listaDeTarefas = document.getElementById('lista-tarefas') ;
+// const btnDelet = document.querySelector('.btnDelet')
 
 btnCriarTarefa.addEventListener('click', addTodo);
 
@@ -13,13 +21,15 @@ function addTodo(){
     criaItemLista.innerText = inputTextoTarefa.value;
     inputTextoTarefa.value=''
     listTodo.appendChild(criaItemLista);
-
-    const creatBtnDelet = document.createElement('button');
-    creatBtnDelet.classList.add('.btnDelet');
-    creatBtnDelet.innerText=""
-    criaItemLista.appendChild(creatBtnDelet)
-
-    
 }
 
-console.log(btnDelet)
+listaDeTarefas.addEventListener('click', mudaCor)
+
+function mudaCor(event) {
+    if(event.target.tagName ==='LI'){
+        for(let i=0; i<listaDeTarefas.children.length; i+=1){
+            listaDeTarefas.children[i].style.backgroundColor=""
+        }
+        event.target.style.backgroundColor="rgb(128, 128, 128)"
+    }
+}
